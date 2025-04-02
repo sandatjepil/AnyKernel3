b7c5a43c8c9f05453b26825a27e2d2f73f4456ee
Agus Suristyono <sandatjepil@proton.me>
2025-04-02 15:40:15 +0700

    Re-Apply KernelSU Toggle
    Signed-off-by: Agus Suristyono <sandatjepil@proton.me>

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -135,6 +135,23 @@ fi
 # android_ver=$(file_getprop /system/build.prop ro.build.version.release);
 # patch_cmdline androidboot.version androidboot.version=$android_ver
 
+# KernelSU Toggle Support
+if [ "`$BB grep -w "selected.1=1" /tmp/aroma-data/spectrum.prop`" ];then
+	if [ "$REG" = "IDN" ];then
+		ui_print "- KernelSU dihidupkan !";
+	elif [ "$REG" = "EN" ];then
+		ui_print "- KernelSU enabled !";
+	fi;
+	patch_cmdline kernelsu.enabled kernelsu.enabled=1
+else
+	if [ "$REG" = "IDN" ];then
+		ui_print "- KernelSU dimatikan !";
+	elif [ "$REG" = "EN" ];then
+		ui_print "- KernelSU disabled !";
+	fi;
+	patch_cmdline kernelsu.enabled kernelsu.enabled=0
+fi;
+
 # Overclock CPU & GPU
 if [ "`$BB grep -w "selected.1=1" /tmp/aroma-data/refrate.prop`" ];then
 	if [ "$REG" = "IDN" ];then
